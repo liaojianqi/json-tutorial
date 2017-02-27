fe5f6272fbd4a6410090bd9bc0cf8eb2d45afa27
liaojianqi <loinliao@163.com>
2017-02-27 18:38:09 +0800

    tutorial01 启程

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -20,13 +20,28 @@ static int test_pass = 0;
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
+//能否正确解析null
 static void test_parse_null() {
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
-
+//能否正确解析true
+static void test_parse_true() {
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, " true "));
+    EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
+}
+//能否正确解析false
+static void test_parse_false() {
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, " false "));
+    EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
+}
+//只有空格或空串
 static void test_parse_expect_value() {
     lept_value v;
 
@@ -38,7 +53,7 @@ static void test_parse_expect_value() {
     EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
-
+//不合理的值
 static void test_parse_invalid_value() {
     lept_value v;
     v.type = LEPT_FALSE;
@@ -49,16 +64,27 @@ static void test_parse_invalid_value() {
     EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "?"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
-
+//多个值
 static void test_parse_root_not_singular() {
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "null x"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
-
+/* 
+ * 只能解析形如"ws value ws"形式的json串
+ * 解析成功返回LEPT_PARSE_OK
+ * 只有空格返回LEPT_PARSE_EXPECT_VALUE
+ * 不合理的值返回LEPT_PARSE_INVALID_VALUE
+ * 有多个值返回LEPT_PARSE_ROOT_NOT_SINGULAR
+ *
+ * 解析成功的类型即为解析得到的值的类型
+ * 解析失败v的类型为LEPT_NULL
+ */
 static void test_parse() {
     test_parse_null();
+    test_parse_true();
+    test_parse_false();
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
